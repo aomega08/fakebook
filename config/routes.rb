9155f392380fc8926187b8f7d98884f724790277
Francesco Boffa <fra.boffa@gmail.com>
2017-01-17 20:23:43 +0100

    Removed show route for Recipient

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'payments#index'
 
-  resources :recipients, only: [:index, :show, :new, :create]
+  resources :recipients, only: [:index, :new, :create]
   resources :payments, only: [:show, :new, :create]
 end
-
